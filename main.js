6d7e181b77f6327208d9162f753a0817435bc270
Léo Lhomme <leo@utopios.solutions>
2025-04-15 10:03:17 +0200

    ini

---
+++ main.js
@@ -0,0 +1,11 @@
+const date22 = new Date("April 22, 2025 12:00:00").getTime();
+const dateNow = Date.now()
+let milirestant = date22  - dateNow
+
+let div = document.querySelector("div")
+
+setInterval(()=>{
+    milirestant-= 113
+    div.innerText = milirestant
+},113)
+
